9bccf71c7c0f9fe82dbb8f6a220a17662c6af848
zknetpor <zknetpor@gmail.com>
2026-03-24 10:03:51 -0700

    Create admin orders management page

---
+++ app/admin/orders/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AdminOrdersPage = () => {
+  return (
+    <div>
+      <h1>Admin Orders Management</h1>
+      {/* Implement orders management logic here */}
+    </div>
+  );
+};
+
+export default AdminOrdersPage;
