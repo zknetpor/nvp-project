f42c42209a22b13a0396989bdf538ae339b08b78
zknetpor <zknetpor@gmail.com>
2026-03-24 10:04:16 -0700

    Create checkout page component

---
+++ app/checkout/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CheckoutPage = () => {
+    return (
+        <div>
+            <h1>Checkout Page</h1>
+            {/* Add checkout form and related functionality here */}
+        </div>
+    );
+};
+
+export default CheckoutPage;
