8c3c2c6979276928d70b01ed073c8b4fe48568f0
zknetpor <zknetpor@gmail.com>
2026-03-24 10:01:14 -0700

    Create tailwind.config.js with custom theme

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        black: '#000000',
+        white: '#FFFFFF',
+        gray: {
+          light: '#D3D3D3',
+          DEFAULT: '#808080',
+          dark: '#A9A9A9',
+        },
+      },
+      fontFamily: {
+        custom: ['YourCustomFont', 'sans-serif'],
+      },
+    },
+  }
+};
