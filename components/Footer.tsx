6b4b77d3a4b10e49ba518c2beceed707cffa9583
zknetpor <zknetpor@gmail.com>
2026-03-24 10:05:12 -0700

    Add Footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>© {new Date(2026, 2, 24).getFullYear()} Your Company. All rights reserved.</p>
+        </footer>
+    );
+};
+
+export default Footer;
