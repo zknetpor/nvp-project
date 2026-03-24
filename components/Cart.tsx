b5c4ab9cb51110ad18bd70ba451430e6b6c7beb8
zknetpor <zknetpor@gmail.com>
2026-03-24 10:04:45 -0700

    Create Cart component

---
+++ components/Cart.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Cart: React.FC = () => {
+    return (
+        <div>
+            <h1>Shopping Cart</h1>
+            <p>Your items will be displayed here.</p>
+        </div>
+    );
+};
+
+export default Cart;
